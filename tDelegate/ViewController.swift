d169e68f4f2880693bc1095a13c4766247f4f348
tyobigoro <n_nakano@mbm.nifty.com>
2019-12-29 13:29:12 +0900

    c

--- tDelegate/ViewController.swift
+++ tDelegate/ViewController.swift
@@ -8,13 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MyDelegate {
 
+    var viewdata = ViewData()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        var controllers:[UIViewController] = []
+        guard let topController = viewdata.getViewController("TEST") as? TestViewController else { return }
+        topController.myDelegate = self
+        controllers.append(topController)
+        
     }
 
-
+    func myTest() {
+          print("")
+    }
+    
 }
 
+class TestViewController: UITableViewController{
+    
+    var myDelegate: MyDelegate?
+
+    override func viewDidLoad() {
+            super.viewDidLoad()
+     }
+    
+    func doSomething() {
+        myDelegate?.myTest()
+    }
+}
+
+protocol MyDelegate {
+    
+    func myTest()
+    
+}
+
+class ViewData{
+
+  func getViewController(_ text:String) -> UIViewController? {
+    switch text {
+    case "TEST" :
+        let storyboard:UIStoryboard = UIStoryboard(name:"Main",bundle:nil)
+        let TestView = storyboard.instantiateViewController(withIdentifier: "TEST") as! TestViewController
+        return TestView
+
+      default: fatalError("getViewController")
+    }
+  }
+
+}
